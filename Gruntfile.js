d1436728f70e54fb423c95868b477b751cdfb6a1
Rory Sterley <royrsterley@gmail.com>
2015-03-02 17:57:39 -0800

    Add front end deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-simple-mocha');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-browserify');
 
   grunt.initConfig({
     jshint: {
@@ -20,16 +23,46 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-    scripts: {
-      files: ['**/*.js'],
-      tasks: ['jshint', 'simplemocha:all'],
-    options: {
-      spawn: false,
+      scripts: {
+        files: ['**/*.js'],
+        tasks: ['jshint', 'simplemocha:all'],
+        options: {
+        spawn: false,
+        },
+      },
     },
-  },
-},
+    clean: {
+      build: {
+        src: ['build/']
+      }
+    },
+    copy: {
+      build: {
+        expand: true,
+        cwd: 'app/',
+        src: '**/*.html',
+        dest: 'build/',
+        flatten: false,
+        filter: 'isFile'
+      }
+    },
+    browserify: {
+      dev: {
+        src: ['app/js/**/*.js'],
+        dest: 'build/bundle.js'
+      },
+      test: {
+        src: ['test/client_side/*_test.js'],
+        dest: 'test/client_side/test_bundle.js'
+      },
+      options: {
+        transform: ['debowerify']
+      }
+    }
   });
 
   grunt.registerTask('test', ['jshint', 'simplemocha:all']);
   grunt.registerTask('default', ['test']);
+  grunt.registerTask('build', ['clean', 'browserify', 'copy']);
+  grunt.registerTask('build:test', ['browserify:test']);
 };
